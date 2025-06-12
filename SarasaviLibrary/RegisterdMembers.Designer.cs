c482f7926cf5707da488d8bfaff4b476bc9b3555
Dewick75 <dulanjaya.pasindu1130@gmail.com>
2025-06-12 14:42:16 +0530

    Create a login Succcesfull message
    Done with nice message showing

--- SarasaviLibrary/RegisterdMembers.Designer.cs
+++ SarasaviLibrary/RegisterdMembers.Designer.cs
@@ -31,14 +31,18 @@
             this.button2 = new System.Windows.Forms.Button();
             this.Table = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnDeleteMember = new System.Windows.Forms.Button();
+            this.btnAddMember = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Table)).BeginInit();
             this.SuspendLayout();
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(34, 31);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(13, 570);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(197, 41);
             this.button2.TabIndex = 2;
@@ -49,33 +53,80 @@
             // Table
             // 
             this.Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Table.Location = new System.Drawing.Point(13, 109);
-            this.Table.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Table.Location = new System.Drawing.Point(13, 167);
+            this.Table.Margin = new System.Windows.Forms.Padding(4);
             this.Table.Name = "Table";
             this.Table.RowHeadersWidth = 51;
-            this.Table.Size = new System.Drawing.Size(1102, 454);
+            this.Table.Size = new System.Drawing.Size(1102, 383);
             this.Table.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(403, 36);
+            this.label1.Location = new System.Drawing.Point(393, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(370, 36);
             this.label1.TabIndex = 4;
             this.label1.Text = "View Registered Members";
+            // 
+            // btnDeleteMember
+            // 
+            this.btnDeleteMember.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteMember.Location = new System.Drawing.Point(428, 570);
+            this.btnDeleteMember.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDeleteMember.Name = "btnDeleteMember";
+            this.btnDeleteMember.Size = new System.Drawing.Size(197, 41);
+            this.btnDeleteMember.TabIndex = 5;
+            this.btnDeleteMember.Text = "Add Members";
+            this.btnDeleteMember.UseVisualStyleBackColor = true;
+            this.btnDeleteMember.Click += new System.EventHandler(this.btnDeleteMember_Click);
+            // 
+            // btnAddMember
+            // 
+            this.btnAddMember.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddMember.Location = new System.Drawing.Point(797, 570);
+            this.btnAddMember.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddMember.Name = "btnAddMember";
+            this.btnAddMember.Size = new System.Drawing.Size(197, 41);
+            this.btnAddMember.TabIndex = 6;
+            this.btnAddMember.Text = "Delete Members";
+            this.btnAddMember.UseVisualStyleBackColor = true;
+            this.btnAddMember.Click += new System.EventHandler(this.btnAddMember_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(72, 118);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(209, 22);
+            this.txtSearch.TabIndex = 7;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(321, 108);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(197, 41);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // RegisterdMembers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.OldLace;
-            this.ClientSize = new System.Drawing.Size(1128, 598);
+            this.ClientSize = new System.Drawing.Size(1128, 644);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnAddMember);
+            this.Controls.Add(this.btnDeleteMember);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Table);
             this.Controls.Add(this.button2);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "RegisterdMembers";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RegisterdMembers";
@@ -89,5 +140,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView Table;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDeleteMember;
+        private System.Windows.Forms.Button btnAddMember;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
     }
 }
